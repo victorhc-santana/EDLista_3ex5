5c50cc3510b43b5abef08b441f1d0b5a5253bfa2
Victor Hugo <148992463+victorhc-santana@users.noreply.github.com>
2025-02-22 21:57:03 -0300

    Add files via upload

--- src/controller/ClassFibonacci.java
+++ src/controller/ClassFibonacci.java
@@ -6,9 +6,13 @@ public class ClassFibonacci {
 	}
 	
 	public int EncontraPosicao(int n) {
+		//se fibonacci que for chamada é = 1 ou 2, retorna seu valor que é 1
 		if ((n == 1) || (n == 2)) {
 			return 1;
 		}
+		/*se posicao requerida é maior que 2 então chama o calculo da fibonacci
+		 *  chamando a recursao duas vezes
+		 */
 		else {
 			return EncontraPosicao(n-1) + EncontraPosicao(n-2);
 		}
